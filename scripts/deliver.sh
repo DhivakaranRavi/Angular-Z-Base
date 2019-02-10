bd0fc85e37660075ac7147cf36396f0df8b85d9f
dhiva03 <dhivakaran.ret@gmail.com>
2019-02-10 23:41:07 +0530

    jenkins-env-updated

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -1,11 +1,11 @@
 
 echo 'Build starts...'
 set -x
-sudo npm run build --unsafe-perm=true --allow-root
+npm run build
 set +x
 
 set -x
-sudo npm start --unsafe-perm=true --allow-root  &
+npm start &
 sleep 1
 echo $! > .pidfile
 set +x
