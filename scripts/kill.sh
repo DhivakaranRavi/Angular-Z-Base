8111868413ffd630fb7806a1345637f0b2e34998
dhiva03 <dhivakaran.ret@gmail.com>
2019-02-11 20:44:25 +0530

    remove-jenkins-configuration

--- scripts/kill.sh
+++
@@ -1,4 +0,0 @@
-
-echo 'Terminates the "npm start" process using its PID'
-set -x
-kill $(cat .pidfile)
--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-
-set -x
-# npm install --save-dev cross-env
-set +x
-
-echo 'Runing Jest Testing...'
-set -x
-npm test 
